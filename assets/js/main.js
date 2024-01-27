6b9c767acf810af2420dba8f03f1caa8ffb23484
Abdullah Zufar <abdullahzufar83@gmail.com>
2024-01-27 17:28:39 +0530

    Classifeid App Base

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+function showLogInModal(){
+    
+    const loginModal = document.getElementById('login-modal');
+    const bootstrapModal = new bootstrap.Modal(loginModal);
+    bootstrapModal.show();
+
+}
