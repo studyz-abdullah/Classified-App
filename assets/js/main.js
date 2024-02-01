e82f4e218ecb873ab10ed3512cf675f9d2452f42
Abdullah Zufar <abdullahzufar83@gmail.com>
2024-02-01 20:22:29 +0530

    Log In

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,28 @@
-function showLogInModal(){
-    
+function showLogInModal() {
     const loginModal = document.getElementById('login-modal');
     const bootstrapModal = new bootstrap.Modal(loginModal);
     bootstrapModal.show();
+}
 
+function logIn() {
+    let email = document.getElementById('login-email').value;
+    let password = document.getElementById('login-password').value;
+
+    const form = new FormData();
+    form.append('email', email);
+    form.append('password', password);
+
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            const response = request.responseText;
+            if (response === 'Success') {
+                alert('Log In Success');
+            } else {
+                alert(response);
+            }
+        }
+    };
+    request.open('post', 'logInProcesss.php', true);
+    request.send(form);
 }
